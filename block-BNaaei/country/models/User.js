d9e45ccf450282df461932e7ca1c496143d10328
vivek sharma <vivekslim@gmail.com>
2021-08-12 21:02:33 +0530

    a2

---
+++ block-BNaaei/country/models/User.js
@@ -0,0 +1,13 @@
+let mongoose = require('mongoose');
+
+let Schema = mongoose.Schema;
+
+let userSchema = new Schema({
+  name: String,
+  email: String,
+  password: String,
+});
+
+let User = mongoose.model('User', userSchema);
+
+module.exports = User;
